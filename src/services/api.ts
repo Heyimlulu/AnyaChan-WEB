a680851e68a118bc41e1ac4dc19139f418de96e4
Lucas <61554619+Heyimlulu@users.noreply.github.com>
2022-11-27 20:18:36 +0100

    Add custom httpAgent

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,9 +1,20 @@
 import axios from 'axios';
+import https from "https";
+
+const instance = axios.create({
+    httpAgent: new https.Agent({
+        rejectUnauthorized: false
+    })
+});
 
 class Api {
 
     static getBotStats() {
-        axios.get('http://150.136.137.28:7700/api/stats').then(response => {
+        instance.get('http://150.136.137.28:7700/api/stats', {
+            headers: {
+                'Access-Control-Allow-Origin': '*',
+            },
+        }).then(response => {
             return response.data;
         }).catch(error => {
             console.log(error);
